5dfbdc7a05ef03e1ae3abed9d38d354610a83bb4
GIGIGUN <sprewell20051116@gmail.com>
2017-08-14 22:51:19 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ sprewell20051116/03_JavaScript_Essentials/index.js
@@ -0,0 +1,25 @@
+var a, b;
+a = 'hello';
+b = a + ' world';
+// b = 'hello world'
+console.log(b);
+
+a = 1;
+b= a + 5.5
+console.log(b);
+
+//
+// Subject03 - 03
+//
+var a = 30;
+console.log(a);
+
+// dollar sign as the variable name is allowed
+var $_a = 1;
+console.log($_a);
+
+//
+// Subject03 - 04
+//
+var z = 10 + 4 - 2;
+console.log(z); // 12
