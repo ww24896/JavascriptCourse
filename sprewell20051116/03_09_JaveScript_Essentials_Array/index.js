97aa554223ff2f89edaaf17ca6df5d850446cbda
GIGIGUN <sprewell20051116@gmail.com>
2017-08-15 23:29:37 +0800

    Complete SUbject_03_Class_09
    Arrays and Array Methods

---
+++ sprewell20051116/03_09_JaveScript_Essentials_Array/index.js
@@ -0,0 +1,21 @@
+console.log("= Subject09 = Array and Array Methods ====");
+
+var points = [25, 16, 7 , 9 , 31];
+console.log(points);
+
+console.log("points.push(8);");
+points.push(8);
+console.log(points);
+
+var points = [25, 16, 7 , 9 , 31];
+console.log("points.pop()");
+console.log(points.pop());
+
+var points = [25, 16, 7 , 9 , 31];
+var first = points[0];
+console.log("points[0];");
+console.log(first);
+
+var points = [25, 16, 7 , 9 , 31];
+console.log("points.length");
+console.log(points.length);
