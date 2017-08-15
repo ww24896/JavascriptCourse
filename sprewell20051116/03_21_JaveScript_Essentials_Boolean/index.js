5fd89e2275edcab74598fdaea01fa06597db7979
GIGIGUN <sprewell20051116@gmail.com>
2017-08-15 23:44:35 +0800

    Complete Subject_03_Class_21
    if statement

--- sprewell20051116/03_21_JaveScript_Essentials_Boolean/index.js
+++ sprewell20051116/03_21_JaveScript_Essentials_Boolean/index.js
@@ -1,21 +1,40 @@
-console.log("= Subject09 = Array and Array Methods ====");
+console.log("= Subject21 = Comparison Operators ====");
 
-var points = [25, 16, 7 , 9 , 31];
-console.log(points);
+var a = false;
+console.log(typeof a);
+console.log(a);
 
-console.log("points.push(8);");
-points.push(8);
-console.log(points);
+console.log("5 == 5");
+var b = 5 == 5;
+console.log(b);
 
-var points = [25, 16, 7 , 9 , 31];
-console.log("points.pop()");
-console.log(points.pop());
+console.log("5 == 7");
+var b = 5 == 7;
+console.log(b);
 
-var points = [25, 16, 7 , 9 , 31];
-var first = points[0];
-console.log("points[0];");
-console.log(first);
+console.log("5 >= 7");
+var b = 5 >= 7;
+console.log(b);
 
-var points = [25, 16, 7 , 9 , 31];
-console.log("points.length");
-console.log(points.length);
+var x = 10;
+
+if (8 > x) {
+  console.log("8 is greater");
+}  else if (15 > x){
+  console.log("15 is greater");
+} else {
+  console.log("8 is not greater");
+}
+
+
+var check = function (number) {
+  if (number % 2 == 0) {
+    console.log(number + " is even!");
+  } else {
+    console.log(number + " is odd!");
+  }
+}
+
+check(4);
+check(7);
+check(10);
