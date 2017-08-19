2f27a35791955a3159c07f3be30ed1f354f0c7e1
GIGIGUN <sprewell20051116@gmail.com>
2017-08-19 10:09:06 +0800

    Complete Subject 04 Class 37
    Babel integration

--- sprewell20051116/04_01_Setting_Up_ES6_Dev_Environment/app/index.js
+++ sprewell20051116/04_01_Setting_Up_ES6_Dev_Environment/app/index.js
@@ -1,1 +1,2 @@
-console.log('Hello World');
+console.log('Hello World --- test automatically update');
+console.log("this is second line");
--- sprewell20051116/04_01_Setting_Up_ES6_Dev_Environment/webpack.config.js
+++ sprewell20051116/04_01_Setting_Up_ES6_Dev_Environment/webpack.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
   entry: ['./app/index.js'],
   output: {
+    // path: 'build',
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
+  },
+// Adding babel to Webpack
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // a regular expression that catches .js files
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'build'),
+    inline: true
   }
 }
