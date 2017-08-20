7812dcf443b1b33dc09f001ef3e4760e8e82ee6e
GIGIGUN <sprewell20051116@gmail.com>
2017-08-20 23:43:31 +0800

    Finish Class 39

--- sprewell20051116/05_JavaScript_Essentials_ES6/app/index.js
+++ sprewell20051116/05_JavaScript_Essentials_ES6/app/index.js
@@ -1,1 +1,25 @@
 console.log("Hi!!!!!!! This is 05_JavaScript_Essentials_ES6!!!");
+var limit = 100;
+console.log(limit);
+limit += 100;
+console.log(limit);
+
+// use let keywords
+console.log("use let keywords");
+let limit_let = 100;
+console.log(limit_let);
+// limit_let += 100;
+limit_let = 200;
+console.log(limit_let);
+
+console.log("use consts keywords");
+const limit_const = 100;
+console.log(limit_const);
+// limit_let += 100;
+// limit_const = 200;
+// console.log(limit_const);
+
+const emails = ["email1", "email2", "email3"];
+// emails = []; // no re-assign
+emails.push("email4") // however you can use "push()" to add elemets" ["email1", "email2", "email3", "email4"];
+console.log(emails);
